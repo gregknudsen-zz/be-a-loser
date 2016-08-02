7defce868b994de56cfc2a167c6c5d66fc2d2c20
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-01 21:53:37 -0400

    having an issue getting videos to display. Will keep working on this

--- js/owl-custom.js
+++ js/owl-custom.js
@@ -4,14 +4,24 @@
 //   });
 // });
 
-$().ready(function($) {	  
- $("#owl-demo").owlCarousel({		
-	navigation : true,
-	slideSpeed : 300,
-	paginationSpeed : 400,
-	autoPlay : false,
-	singleItem:true
- });
+$(function() {	  
+ 	$("#owl-demo").owlCarousel({		
+		 navigation : true,
+		// items: 1,
+		// slideSpeed : 300,
+		// paginationSpeed : 400,
+		// autoPlay : false,
+		// singleItem: true,
+		video: true,
+		responsive:{
+      480:{
+        items:2
+      },
+      600:{
+        items:4
+      }
+    }
+ 	});
 
  $("#owl-demo2").owlCarousel({
 	items : 4,
