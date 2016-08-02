258ba4753651171fd32082f2276b0dba453374b9
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-02 16:46:36 -0400

    begin process of using php to create site

---
+++ includes/navigation.php
@@ -0,0 +1,15 @@
+<?php 
+	<header>
+         <div class="line">
+            <div class="box">
+               <div class="s-6 l-2">
+                  <img src="img/logo2.png">
+               </div>
+               <div id="title">
+                  <h1>Be A Loser</h1>
+               </div>
+            </div>
+         </div>
+
+
+?>
