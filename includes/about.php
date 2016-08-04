ad43c4d94aa91fb03d1fb1c9dc7bff066a14cadd
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-03 22:24:10 -0400

    add about.php and make update box scrollable

---
+++ includes/about.php
@@ -0,0 +1,12 @@
+<div class="line">
+   <div class="margin">
+      <div class="s-12 margin-bottom">
+         <div class="box">
+            <h2>About</h2>
+            <p>
+               Be a Loser started when two fat guys got together and didn't eat for four years. Then they got skinny. Yay, fasting!!
+            </p>
+         </div>
+      </div>
+   </div> 
+</div>
--- index.php
+++ index.php
@@ -27,48 +27,35 @@
       <section>
          <!-- CAROUSEL -->  
       <?php include "includes/carousel.php"; ?>
-         <!-- HOME PAGE BLOCK -->      
-         <div class="line">
-            <div class="margin">
-               <div class="s-12 margin-bottom">
-                  <div class="box">
-                     <h2>About</h2>
-                     <p>
-                        Be a Loser started when two fat guys got together and didn't eat for four years. Then they got skinny. Yay, fasting!!
-                     </p>
-                  </div>
-               </div>
-               <!-- <div class="s-12 m-6 l-4 margin-bottom">
-                  <div class="box">
-                     <h2>Company</h2>
-                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                  </div>
-               </div>
-               <div class="s-12 m-12 l-4 margin-bottom">
-                  <div class="box">
-                     <h2>Services</h2>
-                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                  </div>
-               </div>
-            </div> -->
-         </div>
+         <!-- ABOUT -->      
+      <?php include "includes/about.php"; ?>   
          <!-- ASIDE NAV AND CONTENT -->
          <div class="line">
             <div class="box margin-bottom">
                <div class="margin">
                   <!-- CONTENT -->
-                  <article class="s-12 m-7 l-8">
+                  <article class="s-12 l-6">
                      <h1>The Latest</h1>
-                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
-                        dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
-                        lobortis nisl ut aliquip ex ea commodo consequat.
-                     </p>
-                     <h3>Sub Header</h3>
-                     <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore 
-                        eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril 
-                        delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue 
-                        nihil imperdiet doming id quod mazim placerat facer possim assum. 
-                     </p>
+                     <div class="scrollable">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                           lobortis nisl ut aliquip ex ea commodo consequat.
+                           nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                        nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                        nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                        nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                        nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                        nibh euismod tincidunt ut laoreet 
+                           dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                        
+
+                        </p>
+                     </div>
                   </article>
                   <!-- ASIDE NAV -->
                   
