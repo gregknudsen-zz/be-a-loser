e03d7edea752581f125067f55be43a7556e0de68
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-04 12:37:47 -0400

    add sample updates and css organization

--- includes/updates.php
+++ includes/updates.php
@@ -6,14 +6,16 @@
            <h1>The Latest</h1>
            <div class="scrollable padding">
               <div id="update-content">
-              	<h3>Sample Update</h3>
+              	<h4>Sample Updates</h4>
               	<p>Monday, June 21</p>
-              	<hr class="margin">
               	<p>Be A Loser YouTube Channel created</p>
+              	<hr class="update-divider">
+              	<p>Wednesday, June 23</p>
+              	<p>First video uploaded</p>
+              	<hr class="update-divider">
               </div>
            </div>
         </article>
-      
       <!-- Fat to thin logo -->
       <div id="fat-to-thin" class="s-12 l-6">
          <img src="img/logo.jpg" alt="">      
