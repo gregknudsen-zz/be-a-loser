13c2a6da0e28c14a3535c1873d7dfd974faf3b87
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-04 23:31:49 -0400

    change header to include fat to thin and lose el-boy. This shit was brute forced. Will need to go back and do this using pure RWD

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <header>
    <div class="line">
       <div id="header" class="box">
-         <div class="s-6 l-2">
+         <!-- <div class="s-6 l-2">
             <img id="el-boy" src="img/logo2.png">
-         </div>
+         </div> -->
          <div id="title-logo">
             <img id="fat-to-thin" class="s-12 center" src="img/logo.jpg" alt="" />
             <h1 id="title" class="s-12 center">Be A Loser</h1>
