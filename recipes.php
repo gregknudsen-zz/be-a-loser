d63bb9caaf32f75bba20f0bd209a0aabe88809c9
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-05 16:21:37 -0400

    more organization of files - add CNAME for launch

--- recipes.php
+++ recipes.php
@@ -1,16 +1,21 @@
 <html lang="en-US">
-   <?php include "includes/head.php"; ?>
+   <?php include "./includes/head.php"; ?>
+   
+   <!-- Content -->
+   
    <body class="size-960">
+      
       <!-- HEADER -->
-      <?php include "includes/header.php"; ?>
+      <?php include "./includes/header.php"; ?>
        
       <!-- TOP NAV -->  
-      <?php include "includes/navigation.php"; ?>      
+      <?php include "./includes/navigation.php"; ?>      
       
+      <!-- Coming Soon -->
+      <?php include "./includes/coming-soon.php"; ?>      
       
-      <?php include "includes/coming-soon.php"; ?>      
       <!-- FOOTER -->
-      <?php include "includes/footer.php"; ?>
+      <?php include "./includes/footer.php"; ?>
 
       <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>  
       <script type="text/javascript" src="js/owl-custom.js"></script>  
