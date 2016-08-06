820118ee2b5201fb6ed5134fa8d65eff6f563c2c
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-06 14:11:12 -0400

    add blockquote to about section

--- includes/about.php
+++ includes/about.php
@@ -4,7 +4,13 @@
          <div id="about" class="box">
             <h2>About</h2>
             <p>
-               After my own struggles with weight gain and obesity, as well as pre diabetic blood labs I knew I had to make a change. The standard methods for weight loss, ie. Eat Less, Move More, just weren't working. I wasn't happy with the direction my doctor and mainstream medical and nutritional experts were taking me. So I decided to take matters in my own hands. I discovered the writings of Gary Taubes and Dr. Jason Fung and my quality of life improved immensely. This change was so profound, and after much intense study, so obvious and logical, that I felt I needed to share it with as many people as I could. I felt that YouTube was the best forum to reach as many people as I could as quickly as possible. While the website is still very much a work in progress, you will start to see more content as more videos go up on our website. There will be recipes, suggested reading, and more if we think it will help you on your journey. So add us to your bookmarks, and please continue to check in.
+               After my own struggles with weight gain and obesity, as well as pre diabetic blood labs I knew I had to make a change. The standard methods for weight loss, ie. Eat Less, Move More, just weren't working. I wasn't happy with the direction my doctor and mainstream medical and nutritional experts were taking me. So I decided to take matters in my own hands. I discovered the writings of Gary Taubes and Dr. Jason Fung and my quality of life improved immensely. This change was so profound, and after much intense study, so obvious and logical, that I felt I needed to share it with as many people as I could. I felt that YouTube was the best forum to reach as many people as I could as quickly as possible.
+            </p>
+            <blockquote>
+               "The standard methods for weight loss, ie. Eat Less, Move More, just weren't working."
+            </blockquote> 
+            <p>
+               While the website is still very much a work in progress, you will start to see more content as more videos go up on our website. There will be recipes, suggested reading, and more if we think it will help you on your journey. So add us to your bookmarks, and please continue to check in.
             </p>
             <br>
             <p>
