525b51e4364ab175d4cac93b75b07fe040c1e46c
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-23 14:34:52 -0400

    add a few updates as well as modify positioning of updates relative to the social media icons

--- includes/updates.php
+++ includes/updates.php
@@ -2,10 +2,22 @@
   <div class="box margin-bottom">
      <div class="margin">
         <!-- Updates -->
-        <article class="s-12 l-6">
+        <article class="s-12 l-7">
            <h1>The Latest</h1>
            <div class="scrollable padding">
               <div id="update-content">              
+                <section>
+                  <p>8.23.2016</p>
+                  <h3>Video uploaded!</h3> 
+                  <p>Check out Part 8 of our Intermittent Fasting Series where John let's you know  the advantages Intermittent Fasting will give you in your daily life.</p>
+                  <p>Find it <a href="https://www.youtube.com/watch?v=0q_vy5gFpiY" target="_blank">here</a> on our YouTube channel.</p>
+                </section>
+                <section>
+                  <p>8.23.2016</p>
+                  <h3>Video uploaded!</h3> 
+                  <p>Check out Part 7 of our Intermittent Fasting Series as John discusses Questions and Side Effects of Fasting.</p>
+                  <p>Find it <a href="https://www.youtube.com/watch?v=2u_PRiwkYc" target="_blank">here</a> on our YouTube channel.</p>
+                </section>
                 <section>
                   <p>8.17.2016</p>
                 	<h3>Video uploaded!</h3> 
@@ -32,7 +44,7 @@
            </div>
         </article>
       <!-- Social Media -->
-      <div id="social-media" class="s-12 l-6">
+      <div id="social-media" class="s-12 l-5">
          <a href="https://www.facebook.com/bealosertoday/?ref=aymt_homepage_panel" target="_blank"><img src="img/loser-facebook.jpg" alt=""></a>
          <a href="https://www.youtube.com/channel/UCR2z_y7rQO3CMb3sQe9a48Q" target="_blank"><img src="img/loser-youtube.jpg" alt=""></a>
       </div>
