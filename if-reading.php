42f20be6cac2bf246554a7b2c1f1be41be57d63d
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-23 10:51:39 -0400

    testing Amazon link. Will probably put this content into an include'

--- if-reading.php
+++ if-reading.php
@@ -12,7 +12,8 @@
       <?php include "./includes/navigation.php"; ?>      
       
       <!-- Coming Soon -->
-      <?php include "./includes/coming-soon.php"; ?>      
+      <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=1771641258&asins=1771641258&linkId=3d4ad7e580d134aa4a280d833e70790d&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=fff">
+      </iframe>
       
       <!-- FOOTER -->
       <?php include "./includes/footer.php"; ?>
