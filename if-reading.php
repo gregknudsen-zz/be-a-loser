9d3b6b946a7ec4634de84708bf7b256bbf5771ff
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-23 23:12:51 -0400

    incorporate a partial for recomennded reading. Now trying to style iframe which is proving more difficult than I would have thought

--- if-reading.php
+++ if-reading.php
@@ -9,11 +9,10 @@
       <?php include "./includes/header.php"; ?>
        
       <!-- TOP NAV -->  
-      <?php include "./includes/navigation.php"; ?>      
+      <?php include "./includes/navigation.php"; ?>
       
-      <!-- Coming Soon -->
-      <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=1771641258&asins=1771641258&linkId=3d4ad7e580d134aa4a280d833e70790d&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=fff">
-      </iframe>
+      <!-- IF Reading -->
+      <?php include "./includes/_reading-fasting.php"; ?>
       
       <!-- FOOTER -->
       <?php include "./includes/footer.php"; ?>
