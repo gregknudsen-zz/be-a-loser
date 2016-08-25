4374ede1107644ce623d18a0366534a9a31aecae
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-25 13:36:05 -0400

    getting closer to positioning amazon links where I want.

--- includes/_reading-fasting.php
+++ includes/_reading-fasting.php
@@ -5,12 +5,10 @@
         <article class="s-12">
         	<p>Here are some books we have found to be invaluable in our extensive research of intermittant fasting.</p>
         	<div class="amazon">
-        		<div>
-							<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=1771641258&asins=1771641258&linkId=3d4ad7e580d134aa4a280d833e70790d&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=fff"></iframe>
-						</div>
-						<div>
-							<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=1771641258&asins=1771641258&linkId=3d4ad7e580d134aa4a280d833e70790d&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=fff"></iframe>
-						</div>
+        		
+							<iframe  marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=1771641258&asins=1771641258&linkId=3d4ad7e580d134aa4a280d833e70790d&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=fff"></iframe>
+							<p>This is a great book</p>
+							<!-- <iframe marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=0307474259&asins=0307474259&linkId=22f6ebe2117afc2aa243d4ae1c2defee&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"></iframe> -->
 					</div>
 				</article>
 		</div>
