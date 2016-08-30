85809e97e64c14935b4d284d13f7c564d54a458c
Gregory Knudsen <dev.artist15@gmail.com>
2016-08-29 22:02:41 -0400

    add another book for lchf reading

--- includes/_reading-lchf.php
+++ includes/_reading-lchf.php
@@ -9,6 +9,8 @@
         		
 						<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=0307474259&asins=0307474259&linkId=d12ec71b23708b630156ddd4203e5632&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff">
   					</iframe>
+            <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=1535163879&asins=1535163879&linkId=65d30201ee4a95318faa177f5a8da36c&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff">
+            </iframe>
 							<!-- <iframe marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bealo06-20&marketplace=amazon&region=US&placement=0307474259&asins=0307474259&linkId=22f6ebe2117afc2aa243d4ae1c2defee&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"></iframe> -->
 					</div>
 				</article>
