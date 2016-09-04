76946fe4f868fd0b127d1c7b19c4b00244a4ac42
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-03 21:44:30 -0400

    add video to carousel

--- includes/carousel.php
+++ includes/carousel.php
@@ -4,6 +4,9 @@
          <a class="owl-video" href="https://www.youtube.com/watch?v=0H5E2t2egyM"></a>
       </div>
       <div class="item-video" data-merge="1">
+         <a class="owl-video" href="https://youtu.be/FCrDux9ZKXA"></a>
+      </div>
+      <div class="item-video" data-merge="1">
          <a class="owl-video" href="https://www.youtube.com/watch?v=g39-28uEta8"></a>
       </div>
    </div>
