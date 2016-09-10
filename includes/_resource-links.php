baccc06c8391b2c51aee3417beb8283455cdc12f
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-10 15:58:53 -0500

    add link images & html for proper styles

--- includes/_resource-links.php
+++ includes/_resource-links.php
@@ -1,8 +1,9 @@
-<section>	
-	
-	<div class="link-box">
-		<div class="lchf-box"><a href="../resources/lchf.php"></a></div>
-		<div class="lchf-box"><a href="../resources/if.php"></a></div>
-	</div>
-
-</section>
+<div class="line">
+	<section class="box">	
+		<h1>Resources</h1>
+		<div class="link-box">
+			<div class="lchf-box"><a href="./resources/lchf/lchf.php"><img src="./img/intermittent-fasting.jpg"></a></div>
+			<div class="lchf-box"><a href="./resources/if/if.php"><img src="./img/lchf-guide.jpg"></a></div>
+		</div>
+	</section>
+</div>
