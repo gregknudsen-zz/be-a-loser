6eeee3a2e1489350e5e17d0a10a31498f2b32812
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-12 11:24:00 -0400

    t a point where routing is becoming an issue, as the included head.php is now using an incorrect path for all the assets within resources folder

--- resources/if/if.php
+++ resources/if/if.php
@@ -1,1 +1,23 @@
-<h1>Intermittent Fasting Resources</h1>
+<html lang="en-US">
+   <?php include "includes/head.php"; ?>
+   <body class="size-960">
+      <!-- HEADER -->
+      <?php include "includes/header.php"; ?>
+       
+      <!-- TOP NAV -->  
+      <?php include "includes/navigation.php"; ?>      
+      <!-- <section> -->
+         <!-- CAROUSEL -->  
+      <?php include "includes/carousel.php"; ?>
+         <!-- UPDATES & LOGO -->
+      <?php include "includes/updates.php"; ?>   
+         <!-- ABOUT -->      
+      <?php include "includes/about.php"; ?>   
+      <!-- FOOTER -->
+      <?php include "includes/footer.php"; ?>
+
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>  
+      <script type="text/javascript" src="js/owl-custom.js"></script>  
+      
+   </body>
+</html>
--- resources/lchf/lchf.php
+++ resources/lchf/lchf.php
@@ -1,1 +1,18 @@
-<h1>LCHF Resources</h1>
+<html lang="en-US">
+   <?php include "../../includes/head.php"; ?>
+   <body class="size-960">
+      <!-- HEADER -->
+      <?php include "../../includes/header.php"; ?>
+       
+      <!-- TOP NAV -->  
+      <?php include "../../includes/navigation.php"; ?>      
+      <!-- <section> -->
+			<h1>LCHF RESOURCES</h1>      
+      <!-- FOOTER -->
+      <?php include "../../includes/footer.php"; ?>
+
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>  
+      <script type="text/javascript" src="js/owl-custom.js"></script>  
+      
+   </body>
+</html>
