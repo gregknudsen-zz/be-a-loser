016b03d2494e01f4ea55dd1e78ddc4aef45b8ee7
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-15 15:39:26 -0400

    change p line height, add Twitter links to Taubes/Fung

--- includes/about.php
+++ includes/about.php
@@ -4,7 +4,7 @@
          <div id="about" class="box">
             <h2>About</h2>
             <p>
-               After my own struggles with weight gain and obesity, as well as pre diabetic blood labs I knew I had to make a change. The standard methods for weight loss, ie. Eat Less, Move More, just weren't working. I wasn't happy with the direction my doctor and mainstream medical and nutritional experts were taking me. So I decided to take matters in my own hands. I discovered the writings of Gary Taubes and Dr. Jason Fung and my quality of life improved immensely. This change was so profound, and after much intense study, so obvious and logical, that I felt I needed to share it with as many people as I could. I felt that YouTube was the best forum to reach as many people as I could as quickly as possible. <br>
+               After my own struggles with weight gain and obesity, as well as pre diabetic blood labs I knew I had to make a change. The standard methods for weight loss, ie. Eat Less, Move More, just weren't working. I wasn't happy with the direction my doctor and mainstream medical and nutritional experts were taking me. So I decided to take matters in my own hands. I discovered the writings of <a href="https://twitter.com/garytaubes?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">Gary Taubes</a> and <a href="https://twitter.com/drjasonfung">Dr. Jason Fung</a> and my quality of life improved immensely. This change was so profound, and after much intense study, so obvious and logical, that I felt I needed to share it with as many people as I could. I felt that YouTube was the best forum to reach as many people as I could as quickly as possible. <br>
             </p>
             <blockquote class="l-6 s-12">
                <p>
