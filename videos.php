5c6d9ef1daf610ffe3ae218e4202d37af00bd619
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-15 11:05:47 -0400

    add videos.php for testing purposes & can only get it working by using iframe src rather than js api. Will continue to work on this

---
+++ videos.php
@@ -0,0 +1,23 @@
+<html lang="en-US">
+   <?php include "./includes/head.php"; ?>
+   
+   
+   <body class="size-960">
+      
+      <!-- HEADER -->
+      <?php include "./includes/header.php"; ?>
+       
+      <!-- TOP NAV -->  
+      <?php include "./includes/navigation.php"; ?>      
+   
+      <!-- YouTube Playlists -->
+      <?php include "./includes/_videos.php"; ?>      
+      
+      <!-- FOOTER -->
+      <?php include "./includes/footer.php"; ?>
+
+      
+      <script type="text/javascript" src="js/youtube-custom.js"></script>  
+      
+   </body>
+</html>
