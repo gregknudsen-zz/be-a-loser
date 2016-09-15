27b143fa2a4874ba1232cbc6099b7d334404b7d3
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-15 16:17:35 -0400

    add slight border radius to carousel

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,5 +1,5 @@
 <div class="line">
-   <div id="owl-demo" class="owl-carousel owl-theme margin-bottom">
+   <div id="owl-demo" class="owl-carousel owl-theme">
       <div class="item-video" data-merge="1">
          <a class="owl-video" href="https://www.youtube.com/watch?v=0H5E2t2egyM"></a>
       </div>
