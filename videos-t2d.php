2049beac0c80f397c5a1ab614f535d8aa7d3cffa
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-16 11:10:15 -0400

    add files for each video series

---
+++ videos-t2d.php
@@ -0,0 +1,24 @@
+<html lang="en-US">
+   <?php include "./includes/head.php"; ?>
+   
+   <!-- Content -->
+   
+   <body class="size-960">
+      
+      <!-- HEADER -->
+      <?php include "./includes/header.php"; ?>
+       
+      <!-- TOP NAV -->  
+      <?php include "./includes/navigation.php"; ?>
+      
+      <!-- Type 2 Diabetes Videos -->
+      <?php include "./includes/_videos-t2d.php"; ?>
+      
+      <!-- FOOTER -->
+      <?php include "./includes/footer.php"; ?>
+
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>  
+      <script type="text/javascript" src="js/owl-custom.js"></script>  
+      
+   </body>
+</html>
---
+++ videos-uwg.php
@@ -0,0 +1,24 @@
+<html lang="en-US">
+   <?php include "./includes/head.php"; ?>
+   
+   <!-- Content -->
+   
+   <body class="size-960">
+      
+      <!-- HEADER -->
+      <?php include "./includes/header.php"; ?>
+       
+      <!-- TOP NAV -->  
+      <?php include "./includes/navigation.php"; ?>
+      
+      <!-- Understanding Weight Gain series -->
+      <?php include "./includes/_videos-uwg.php"; ?>
+      
+      <!-- FOOTER -->
+      <?php include "./includes/footer.php"; ?>
+
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>  
+      <script type="text/javascript" src="js/owl-custom.js"></script>  
+      
+   </body>
+</html>
