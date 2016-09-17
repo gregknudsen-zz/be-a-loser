b3a363fc72b5adc7fb1660a2ea06756bf3efb0fa
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-16 22:31:29 -0400

    continue process of adding playlists

--- includes/_videos-t2d.php
+++ includes/_videos-t2d.php
@@ -5,9 +5,9 @@
 	      <div class="videos border-top">
 	      <div class="h3">
 	      	<h3>Type 2 Diabetes</h3>
+	      <p>Use the controls in the upper left hand corner of the display 
+	      to navigate your way around the videos.</p>
 	      </div>
-	      <!-- <p>Use the controls in the upper left hand corner of the display 
-	      to navigate your way around the videos.</p> -->
 	      	<div id="t2d-player"></div>
 				</div>
 
