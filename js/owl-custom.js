838a2ef0c175e1d2b59449a5f3d025bac83a53fe
Gregory Knudsen <dev.artist15@gmail.com>
2016-09-24 22:55:29 -0400

    add IIFE for carousel function privacy

--- js/owl-custom.js
+++ js/owl-custom.js
@@ -4,12 +4,22 @@
 //   });
 // });
 
-$(function() {	  
- 	$("#owl-demo").owlCarousel({		
-		// nav: true,
-		items: 1,
-		loop: true,
-		video: true,
- 	});
+var Carousel = (function(){
 
-});
+	function init() {	  
+	 	$("#owl-demo").owlCarousel({		
+			// nav: true,
+			items: 1,
+			loop: true,
+			video: true,
+	 	});
+
+	};
+
+	return {
+		init: init
+	}
+
+})();
+
+$(document).ready(Carousel.init);
