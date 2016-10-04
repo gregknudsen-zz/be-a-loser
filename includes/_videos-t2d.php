aafd9931543726e386cdbad570f72e56162f9c4e
Gregory Knudsen <dev.artist15@gmail.com>
2016-10-03 21:56:54 -0400

    adding playlists with minor styles

--- includes/_videos-t2d.php
+++ includes/_videos-t2d.php
@@ -1,15 +1,11 @@
  <div class="line">
   <div class="box">
         
-        <!-- Videos -->
-	      <div class="border-top">
-	      <div class="h3">
-	      	<h3>Type 2 Diabetes</h3>
-	      </div class="videos">
-	      	<div id="t2d-video"></div>
-	      	
-
-				</div>
+    <!-- Videos -->
+    <div class="h3">
+    	<h3>Type 2 Diabetes</h3>
+    </div class="videos">
+    	<div id="t2d-video"></div>
 
 		</div>
 	</div>
