713abcbbdba314a62320dc12fa7ac07e209503ef
Gregory Knudsen <dev.artist15@gmail.com>
2016-10-11 23:10:26 -0400

    rearrange video series order

--- includes/navigation.php
+++ includes/navigation.php
@@ -14,10 +14,10 @@
             <li>
                <a>Video Series</a>
                <ul>
-                  <li><a href="./videos-if.php">Intermittent Fasting</a></li>
                   <li><a href="./videos-uwg.php">Understanding Weight Gain</a></li>
-                  <li><a href="./videos-t2d.php">Type 2 Diabetes</a></li>
+                  <li><a href="./videos-if.php">Intermittent Fasting</a></li>
                   <li><a href="./videos-lchf.php">LCHF</a></li>
+                  <li><a href="./videos-t2d.php">Type 2 Diabetes</a></li>
                </ul>
             </li>
             <li id="youtube"><a id="youtube-nav" href="https://www.youtube.com/channel/UCR2z_y7rQO3CMb3sQe9a48Q" target="_blank">YouTube Channel</a></li>
