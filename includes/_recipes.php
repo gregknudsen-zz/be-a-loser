491380bfaccfe4d5730205be69a3a0ec6388495f
Gregory Knudsen <dev.artist15@gmail.com>
2016-10-17 23:31:13 -0400

    remove table & just use ul for the time being for each recipe

--- includes/_recipes.php
+++ includes/_recipes.php
@@ -1,16 +1,9 @@
 <div class="line">
-	<div class="recipe-list">
-	<table>
-		<tr>
-			<th><h1>Recipes</h1></th>
-		</tr>
-		<tr>
-		<td><a href="./img/recipes/BoneBroth.pdf" target="_blank">Bone Broth</a></h3></td>
-		
-		</tr>
-	</table>
-		<!-- <h1>Recipes</h1>
-
-		<h3><a href="./img/recipes/BoneBroth.pdf" target="_blank">Bone Broth</a></h3> -->
+	<div class="recipe-list border-top">
+	
+		<h1>Recipes</h1>
+		<ul>
+			<li><h3><a href="./img/recipes/BoneBroth.pdf" target="_blank">Bone Broth</a></h3></li>
+		</ul>
 	</div>
 </div>
