c152af7c02d5af76e1d585149572f874c1a90daf
Gregory Knudsen <dev.artist15@gmail.com>
2016-10-19 14:38:39 -0400

    add cooking series to site

---
+++ includes/_videos-cooking.php
@@ -0,0 +1,13 @@
+    <!-- Videos -->
+<div class="video-wrapper line">
+	
+	<div class="series-title">
+		<h1>Cooking</h1>
+	</div>
+	
+	<div class="video">
+		<div id="cooking-video"></div>
+	</div>
+
+	<script type="text/javascript" src="js/videos/cooking.js"></script>
+</div>
