88174d8c85611937747293514fb5c6f814d25e72
Gregory Knudsen <dev.artist15@gmail.com>
2016-11-18 23:39:09 -0500

    add another recipe & add link to cooking playlist

--- includes/_recipes.php
+++ includes/_recipes.php
@@ -3,7 +3,9 @@
 	
 		<h1>Recipes</h1>
 		<ul>
-			<li><h3><a href="./img/recipes/BoneBroth.pdf" target="_blank">Bone Broth</a></h3></li>
+			<li><h3><a href="./img/recipes/BoneBroth.pdf" target="_blank">Bone Broth*</a></h3></li>
+			<li><h3><a href="./img/recipes/Nutmeal.pdf" target="_blank">Nutmeal*</a></h3></li>
 		</ul>
+		<p>* Can be found in cooking playlist <a href="./includes/_videos-cooking.php">here</a></p>
 	</div>
 </div>
